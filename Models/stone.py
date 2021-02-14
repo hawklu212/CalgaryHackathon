5c5cfae5bff7b9ceda9da8965d22cbe41d93a7b0
hawklu212 <54925268+hawklu212@users.noreply.github.com>
2021-02-13 16:43:47 -0800

    Add files via upload

---
+++ Models/stone.py
@@ -0,0 +1,7 @@
+class stoneRock:
+    def __init__(self,xcoord,ycoord,radius,color):
+        self.xcoord =xcoord;
+        self.ycoord = ycoord;
+        self.radius = radius;
+        self.color = color;
+
