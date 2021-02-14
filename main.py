025be6272001a37b77af5023a977bdf561bc4b71
hawklu212 <54925268+hawklu212@users.noreply.github.com>
2021-02-13 16:42:44 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,53 +1,59 @@
 import pygame
-
+from Models.stone import stoneRock
+from Models.Player import playerSprite
+#from Models.circle import circle
+import Pictures
 pygame.init()
 
+win = pygame.display.set_mode((500, 500));
+bear = pygame.image.load("Pictures/Mock1.png")
+swbear = pygame.image.load("Pictures/Mock_Sweeper_1.png")
+smbear = pygame.image.load("Pictures/Mock_Sweeper_2.png")
+rect = bear.get_rect()
+pygame.display.set_caption("First Game")
+clock = pygame.time.Clock()
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+run = True
 
-class ThrowingBear:
-    """This is a throwing bear class where the bear throws the curling stone"""
+while run:
+   # pygame.time.delay(
+    #    20)  # This will delay the game the given amount of milliseconds. In our casee 0.1 seconds will be the delay
 
-    def __init__(self, xposition):
-        # Class attribute
-        size = 100
+    for event in pygame.event.get():  # This will loop through a list of any keyboard or mouse events.
+        if event.type == pygame.QUIT:  # Checks if the red button in the corner of the window is clicked
+            run = False  # Ends the game loop
+    keys = pygame.key.get_pressed()
+    mouse = pygame.mouse.get_pressed()
 
-        self.xposition = xposition
+    if keys[pygame.K_LEFT]:  # We can check if a key is pressed like this
+        x-= vel/2
+    if keys[pygame.K_RIGHT]:
+        x += vel/2
+    if keys[pygame.K_UP]:
+        y -= vel
 
+    if keys[pygame.K_DOWN]:
+        y += vel
+    if mouse[0]:
+        y-= vel
 
-class SweepingBear:
-    """This is a sweeping bear class where the bear sweeps the ice"""
+    win.fill((255, 255, 255))
+    #win.blit(bear, rect)
+    #pygame.draw(win, bear, rect, 1)
+    bear1 = pygame.transform.scale(swbear, (100, 100))
+    bear2 = pygame.transform.scale(smbear, (100, 100))
+    win.blit(bear1, (x, y))
+    pygame.draw.circle(win, (255, 0, 0), (400, 50), 20)
+    pygame.draw.circle(win, (255, 0, 0), (x, y), 20)  # This takes: window/surface, color, rect
+    pygame.display.update()
+    pygame.time.delay(10);
+    win.blit(bear2, (x, y))
+    pygame.draw.circle(win, (0, 255, 0), (x, y), 20)
+    pygame.display.update()  # This updates the screen so we can see our rectangle
+    pygame.time.delay(10);
 
-    def __init__(self, xposition, yposition, speed):
-        # Class attribute
-        size = 100
-
-        self.xposition = xposition
-        self.yposition = yposition
-        self.speed = speed
-
-
-class Stone:
-    """This is a curling stone class"""
-
-    def __init__(self, xposition, yposition, speed, angle):
-        radius = 20
-
-        self.xposition = xposition
-        self.yposition = yposition
-        self.angle = angle
-        self.speed = speed
-
-    def stoneCollison(self, stone, radius):
-        if self.calculateDistance(stone) >= (radius * 2) ** 2:
-            return True;
-            return False
-
-    def calculateDistance(self, stone1, stone2):
-        ((stone1.xposition - stone2.xposition) ** 2) + ((stone1.yposition - stone2.yposition) ** 2)
-        # a^2 + b^2
-
-
-class Scoreboard:
-    """This is a scoreboard class"""
-
-    def __init__(self, score):
-        self.score = score
+pygame.quit()  # If we exit the loop this will execute and close our game
